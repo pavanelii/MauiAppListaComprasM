c35458b8c2c8a1247ee69e7d2e4a2c6ebcf7f648
Maria Eduarda Pavaneli Lira <mariaeduardapavanelilira@gamil.com>
2024-02-26 11:38:58 -0300

    Ajustando DataBaseHelper

--- MauiAppListaComprasM/Helpers/SQLiteDataBaserHelper.cs
+++ MauiAppListaComprasM/Helpers/SQLiteDataBaserHelper.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MauiAppListaComprasM.Models;
+using SQLite;
 
 namespace MauiAppListaComprasM.Helpers
 {
-    internal class SQLiteDataBaserHelper
+    public class SQLiteDataBaserHelper
     {
+        readonly SQLiteAsyncConnection _conn;
+    
+        public SQLiteAsyncConnection(string path)
+        {
+            _conn = new SQLiteAsyncConnection(path);
+            _conn.CreateTableAsync<Produto>().Wait();
+        }
+    
+        public Task<int> insert(Produto p)
+        {
+            return _conn.InsertAsync(p);
+        }
+    
+    
+    
+    
     }
+
+
 }
