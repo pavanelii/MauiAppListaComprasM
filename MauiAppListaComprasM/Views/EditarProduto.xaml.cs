a69a8ee848b1cf6fa74db2d00df4b86d6bdc144b
Usuario <Usuario@CPD03-M08>
2024-03-11 12:23:18 -0300

    Edição

---
+++ MauiAppListaComprasM/Views/EditarProduto.xaml.cs
@@ -0,0 +1,34 @@
+using MauiAppListaComprasM.Models;
+using Microsoft.UI.Xaml.Automation.Peers;
+
+namespace MauiAppListaComprasM.Views;
+
+public partial class EditarProduto : ContentPage
+{
+	public EditarProduto()
+	{
+		InitializeComponent();
+	}
+
+    private async void ToolbarItem_Clicked(object sender, EventArgs e)
+    {
+		try
+		{
+			Produto produto_anexado = BindingContext as Produto;
+
+			Produto p = new Produto
+			{
+				Id = produto_anexado.Id,
+				Descricao = txt_descrição.Text,
+				Quantidade = Convert.ToDouble(txt_quantidade.Text),
+				Preco = Convert.ToDouble(txt_preco.Text),
+			};
+
+			await App.Db.Update(p);
+			await DisplayAlert("Sucesso!", "Produto Editado!", "OK");
+		} catch(Exception ex) 
+		{
+			await DisplayAlert("Ops", ex.Message, "Fechar"); 
+		};
+    }   
+}
