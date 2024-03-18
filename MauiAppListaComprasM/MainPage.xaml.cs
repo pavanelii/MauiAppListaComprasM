1d13dd520358438a0b45d99b0ea9cf8f1c38694d
Usuario <Usuario@CPD03-M03>
2024-03-18 12:27:10 -0300

    Revisão e Edição

--- MauiAppListaComprasM/MainPage.xaml.cs
+++ MauiAppListaComprasM/MainPage.xaml.cs
@@ -1,25 +1,105 @@
-﻿namespace MauiAppListaComprasM
+﻿using MauiAppListaComprasM.Models;
+using System.Collections.ObjectModel;
+
+namespace MauiAppListaComprasM
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
+        ObservableCollection<Produto> lista_produtos =
+            new ObservableCollection<Produto>();
+        
         public MainPage()
         {
             InitializeComponent();
+            lst_produtos.ItemsSource = lista_produtos;
         }
 
-        private void OnCounterClicked(object sender, EventArgs e)
+        private void ToolbarItem_Clicked_Somar(object sender, EventArgs e)
         {
-            count++;
+            double soma = lista_produtos.Sum(i => (i = Preco * i.Quantidade));
+            string msg = $"O total é {soma:C}";
+            DisplayAlert("Somatória", msg, "Fechar");
+        }
 
-            if (count == 1)
-                CounterBtn.Text = $"Clicked {count} time";
-            else
-                CounterBtn.Text = $"Clicked {count} times";
+        protected override async void OnAppearing()
+        {
+            if (lst_produtos.Count == 0) 
+            { 
+                Task.Run(async ()) ==>
+                {
+                    List<Produto> tmp = await App.Db.GetAll();
+                    foreach (Produto p  in tmp) 
+                    { 
+                        lista_produtos.Add(p);
+                    }
+                });
+            }
+        }
+        
+        private async void ToobarItem_Clicked_Add(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("/NovoProduto");
+        }
 
-            SemanticScreenReader.Announce(CounterBtn.Text);
+        private void ref_carregando_Refreshing(object sender, EventArgs e)
+        {
+            lista_produtos.Clear();
+            Task.Run(async () =>
+            {
+                List<Produto> tmp = await App.Db.Search(q);
+                foreach (Produto p in tmp)
+                {
+                    lista_produtos.Add(p);
+                }
+            });
+            ref_carregando_IsRefreshing = false;
+        }
+
+        private void lst_produtos_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+
+        }
+
+        private async void MenuItem_Clicked_Remover(object sender, EventArgs e)
+        {
+            try
+            {
+                MenuItem selecionado = (MenuItem)sender;
+
+                Produto p = selecionado.BindingContext as Produto;
+                
+                bool confirm = await DisplayAlert(
+                    "Tem certeza?", "Remover Produto?", 
+                    "Sim", "Cancelar");
+                
+                if (confirm)
+                {
+                    await App.Db.Delete(p.Id);
+                    await DisplayAlert("Sucesso!",
+                        "Produto Removido", "OK");
+                }
+            
+            } catch (Exception ex)
+            {
+                
+                
+            
+            }
+        }
+
+        private async void txt_search_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            string q = e.NewTextValue;
+            lista_produtos.Clear();
+            Task.Run(async() ==>
+            {
+                List<Produto> tmp = await App.Db.Search(q);
+                foreach (Produto p in tmp)
+                {
+                    lista_produtos.Add(p);
+                }
+            });
+            
         }
     }
-
 }
---
+++ MauiAppListaComprasM/Views/NovoProduto2.xaml.cs
@@ -0,0 +1,33 @@
+using MauiAppListaComprasM.Models;
+
+namespace MauiAppListaComprasM.Views;
+
+public partial class NovoProduto2 : ContentPage
+{
+	public NovoProduto2()
+	{
+		InitializeComponent();
+	}
+
+    private async void ToolbarItem_Clicked(object sender, EventArgs e)
+    {
+
+        try
+        {
+            Produto p = new Produto
+            {
+                Descricao = txt_descricao.Text,
+                Quantidade = Convert.ToDouble(txt_quantidade.Text),
+                Preco = Convert.ToDouble(txt_preco.Text),
+            };
+
+            await App.Db.Update(p);
+            await DisplayAlert("Sucesso!", "Produto Editado!", "OK");
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Ops", ex.Message, "OK");
+
+        }
+    }
+}
